77b661b89a1d710df9665d56434e21de69ab81be
Tri <67373913+TriDiscord@users.noreply.github.com>
2022-05-28 15:12:03 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,6 +14,12 @@
 # limitations under the License.
 #
 
+# This file is executed by build/envsetup.sh, and can use anything
+# defined in envsetup.sh.
+#
+# In particular, you can add lunch options with the add_lunch_combo
+# function: add_lunch_combo generic-eng
+
 for variant in eng user userdebug; do
   add_lunch_combo omni_viam1-$variant
 done
