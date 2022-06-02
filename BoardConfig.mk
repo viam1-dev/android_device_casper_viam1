b4f9d1fdcaf0940fbf41b86725530f3323e32108
Tri <67373913+TriDiscord@users.noreply.github.com>
2022-06-02 15:30:37 +0300

    Attempt properly compressed ramdisk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,24 +41,24 @@ BOARD_HAS_LARGE_FILESYSTEM         := true
 TW_INCLUDE_CRYPTO                  := true
 
 # Kernel
-TARGET_KERNEL_SOURCE                      := kernel/casper/viam1
-TARGET_KERNEL_CONFIG                      := viam1_defconfig
-TARGET_KERNEL_CROSS_COMPILE_PREFIX        := aarch64-linux-android-
-BOARD_CUSTOM_BOOTIMG_MK                   := $(LOCAL_PATH)/mkbootimg.mk
-BOARD_KERNEL_CMDLINE                      := bootopt=64S3,32N2,64N2
-BOARD_KERNEL_PAGESIZE                     := 2048
-BOARD_KERNEL_BASE                         := 0x40078000
-BOARD_KERNEL_OFFSET                       := 0x00008000
-BOARD_RAMDISK_OFFSET                      := 0x03f88000
-BOARD_SECOND_OFFSET                       := 0x00e88000
-BOARD_TAGS_OFFSET                         := 0x0df88000
-BOARD_KERNEL_IMAGE_NAME                   := Image.gz-dtb
-BOARD_MKBOOTIMG_ARGS                      += --pagesize $(BOARD_KERNEL_PAGESIZE)
-BOARD_MKBOOTIMG_ARGS                      += --base $(BOARD_KERNEL_BASE)
-BOARD_MKBOOTIMG_ARGS                      += --kernel_offset $(BOARD_KERNEL_OFFSET)
-BOARD_MKBOOTIMG_ARGS                      += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
-BOARD_MKBOOTIMG_ARGS                      += --second_offset $(BOARD_SECOND_OFFSET)
-BOARD_MKBOOTIMG_ARGS                      += --tags_offset $(BOARD_TAGS_OFFSET)
+TARGET_KERNEL_SOURCE               := kernel/casper/viam1
+TARGET_KERNEL_CONFIG               := viam1_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+BOARD_NEEDS_LZMA_MINIGZIP          := true
+BOARD_KERNEL_CMDLINE               := bootopt=64S3,32N2,64N2
+BOARD_KERNEL_PAGESIZE              := 2048
+BOARD_KERNEL_BASE                  := 0x40078000
+BOARD_KERNEL_OFFSET                := 0x00008000
+BOARD_RAMDISK_OFFSET               := 0x03f88000
+BOARD_SECOND_OFFSET                := 0x00e88000
+BOARD_TAGS_OFFSET                  := 0x0df88000
+BOARD_KERNEL_IMAGE_NAME            := Image.gz-dtb
+BOARD_MKBOOTIMG_ARGS               += --pagesize $(BOARD_KERNEL_PAGESIZE)
+BOARD_MKBOOTIMG_ARGS               += --base $(BOARD_KERNEL_BASE)
+BOARD_MKBOOTIMG_ARGS               += --kernel_offset $(BOARD_KERNEL_OFFSET)
+BOARD_MKBOOTIMG_ARGS               += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_MKBOOTIMG_ARGS               += --second_offset $(BOARD_SECOND_OFFSET)
+BOARD_MKBOOTIMG_ARGS               += --tags_offset $(BOARD_TAGS_OFFSET)
 
 # Logging
 TARGET_USES_LOGD    := true
@@ -69,4 +69,3 @@ TW_THEME := portrait_hdpi
 
 # TWRP
 TW_EXCLUDE_SUPERSU := true
-TW_EXCLUDE_PYTHON  := true
